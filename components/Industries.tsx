f8fef7e9d58bdc651c6bfe7bc2968c486c6fcd04
Sahil Bhuva <bhuvasahil2025@gmail.com>
2026-08-18 17:54:56 -0400

    Reduce top padding on all sections

--- components/Industries.tsx
+++ components/Industries.tsx
@@ -5,7 +5,7 @@ import Reveal from "./Reveal";
 
 export default function Industries() {
   return (
-    <section id="industries" className="scroll-mt-4 bg-white py-16 sm:py-20">
+    <section id="industries" className="scroll-mt-4 bg-white pt-8 pb-16 sm:pt-10 sm:pb-20">
       <div className="mx-auto max-w-7xl px-6">
         <SectionHeading
           eyebrow="Industries We Serve"
